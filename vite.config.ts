982f1e0845329ea6533b2dac5a28ad607dc12a46
JonatanBadillo <jonatanbadillo.19@gmail.com>
2026-04-26 19:06:48 -0600

    Fix paths in index.html and update target in Vite config for compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/activa-lab/',
+  base: '/activa-lab/', 
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    target: 'esnext' // Dejamos que Vite maneje la compatibilidad estándar
+    target: 'es2020',
   }
 })
