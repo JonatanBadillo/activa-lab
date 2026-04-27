b3371f69cb1c94b38141986873f49df5ea4054f5
JonatanBadillo <jonatanbadillo.19@gmail.com>
2026-04-26 18:20:26 -0600

    Manual deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: '/activa-lab/',
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
